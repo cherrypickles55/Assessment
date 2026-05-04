443f45302996ccc8310da5a0bbc5f3a250539f34
cherrypickles55 <susanths4014@masseyhigh.school.nz>
2026-05-05 10:54:12 +1200

    working on instructions (partial)

---
+++ instructions.py
@@ -0,0 +1,12 @@
+print("Welcome to Basic Facts Quiz!!")
+
+
+
+
+
+# Main routine
+
+# testing loop...
+while True:
+     want_instructions = yes_no("Do you want instructions? ")
+     print(f"you chose {want_instructions}")
