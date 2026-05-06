3474949c4daf98f3090b223000ca4dbd144570cd
cherrypickles55 <susanths4014@masseyhigh.school.nz>
2026-05-06 12:28:27 +1200

    y/n in progress

--- instructions.py
+++ instructions.py
@@ -1,12 +1,77 @@
 print("Welcome to Basic Facts Quiz!!")
 
+print("How many questions would you like?")
+
+# Main Routine
+
+# Intialise game variables
+mode = "regular"
+rounds_played = 0
+rounds_tied = 0
+rounds_lost = 0
+
+bf_list = ["rock", "paper", "scissors", "xxx"]
+game_history = []
 
 
 
 
-# Main routine
 
-# testing loop...
-while True:
-     want_instructions = yes_no("Do you want instructions? ")
-     print(f"you chose {want_instructions}")
+
+
+# Ask user for number of questions
+def int_check(question, exit_code=None):
+    """ checks for an integer more than 0 (allows <enter>)"""
+
+
+    while True:
+        error = "Please enter an integer that is 1 or more."
+
+        response = input(question)
+
+
+        # check for infinite mode / exit code
+        if response == exit_code:
+            return exit_code
+
+
+        try:
+            # tries to make the response into an integer
+            response = int(response)
+
+
+            # checks that the number is more than / equal to 1
+            if response < 1:
+                print(error)
+
+            else:
+                return response
+
+
+        except ValueError:
+            # if the response is not an integer, displays an error
+            print(error)
+
+
+
+
+    while True:
+
+        # Get user response and make sure it's lowercase
+        user_response = input(question).lower()
+
+
+
+        for var_item in valid_ans:
+            # check if the user response is a word in the list
+            if var_item == user_response:
+                return var_item
+
+
+
+            # check if the user response is the same as
+            # the first letter of an item in the list
+            elif user_response == var_item [0]:
+                return var_item
+
+# Displays Instructions
